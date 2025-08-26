9c7f7510d3f84dea7c78acf2925dfd427017fe0c
xion2003 <63761093+xion2003@users.noreply.github.com>
2025-08-25 20:18:54 -0400

    Add files via upload

---
+++ miniapp1.py
@@ -0,0 +1,8 @@
+uname = input("Enter your username: ")
+pword = input("Enter your password: ")
+if uname == ("username") and pword == ("password"):
+    print("Welcome, User")
+elif uname == ("usernamebutbetter") and pword == ("passwordbutbetter"):
+    print("Welcome, Admin")
+else:
+    print("Incorrect username or password.")
